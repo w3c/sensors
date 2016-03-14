0941b46bba7d023b7c96ec2f9d96bcd994e06e2c
Tobie Langel <tobie@codespeaks.com>
2016-03-14 12:00:56 +0100

    Large Geoloc polyfill refactor. WIP.

--- polyfills/geolocation.js
+++ polyfills/geolocation.js
@@ -38,6 +38,144 @@
         return event;
     }
     
+    var _geolocationSensor = (function() {
+        var self = {};
+        var SUPPORTED_REPORTING_MODES = ["auto"];
+        var currentReportingMode = "auto";
+        var associatedSensors = new Set();
+        var state = "idle"; // one of idle, activating, active, deactivating,
+        var currentReading = null;
+        var cachedReading = null;
+        var _promise = null;
+        var _watchId = null;
+        function _enableHighAccuracy() {
+            var enableHighAccuracy = false;
+            associatedSensors.forEach(function(sensor) {
+                if (getSlot(sensor, "options").accuracy == "high") {
+                    enableHighAccuracy = true;
+                }
+            });
+            return enableHighAccuracy
+        }
+        
+        var _options = null;
+        
+        function getOptions() {
+            return {
+                enableHighAccuracy: _enableHighAccuracy(), 
+                maximumAge: 0, 
+                timeout: Infinity
+            };
+        }
+        
+        function hasOptionsChanged(opt) {
+            if (!_options) return true;
+            if (_options.enableHighAccuracy !== opt.enableHighAccuracy) return true;
+            if (_options.maximumAge !== opt.maximumAge) return true;
+            if (_options.timeout !== opt.timeout) return true;
+            return false;
+        }
+        
+        function emitReading(reading, sensor) {
+            sensor.reading = reading;
+            var event = new SensorReadingEvent("reading", {
+                reading: reading
+            });
+            sensor.dispatchEvent(event);
+        }
+        
+        function emitReadingToAll(reading) {
+            // maybe we'd need active and non-active sensors queues?
+            var sensors = Array.from(associatedSensors);
+            sensors.forEach(function(sensor) {
+                emitReading(reading, sensor)
+            });
+        }
+        
+        function register(sensor) {
+            associatedSensors.add(sensor);
+            var opt = getOptions();
+            var optChanged = hasOptionsChanged(opt);
+            _options = opt;
+            var currentState = state;
+            if (currentState == "idle") {
+                state = "activating";
+                activate(opt);
+            } else if (currentState == "activating") {
+                if (optChanged) activate(opt)
+            } else if (currentState == "active") {
+                if (optChanged) {
+                    activate(opt);
+                    // do we resolve the promise immediately here?
+                    // do we set sensor.reading to the current reading?
+                } else {
+                    emitReading(currentReading, sensor); // TODO maybe have an option to force new reading here.
+                    // resolvePromise
+                }
+            }
+        }
+        
+        function deregister(sensor) {
+            associatedSensors.delete(sensor);
+            if (associatedSensors.size == 0) {
+                state = "idle";
+                navigator.geolocation.clearWatch(_watchId);
+                _watchId = null;
+            }
+        }
+        
+        function ondata(position) {
+            var coords = position.coords;
+            var reading = currentReading = new GeolocationSensorReading({
+                accuracy        : coords.accuracy,
+                altitude        : coords.altitude,
+                altitudeAccuracy: coords.altitudeAccuracy,
+                heading         : coords.heading,
+                latitude        : coords.latitude,
+                longitude       : coords.longitude,
+                speed           : coords.speed,
+                timeStamp       : position.timestamp // watch out for the diff casing, here.
+            });
+            if (state == "activating") {
+                state = "active";
+                Array.from(associatedSensors).forEach(function(sensor) {
+                    resolve(sensor, reading);
+                });
+            }
+            emitReadingToAll(reading);
+        }
+        
+        function onerror(err) {
+            emitErrorToAll(err);
+        }
+        
+        function emitErrorToAll(error) {
+            var sensors = Array.from(associatedSensors);
+            sensors.forEach(function(sensor) {
+                var errEvent = new ErrorEvent("error", {
+                    message:  err.message,
+                    filename: err.filename,
+                    lineno:   err.lineno,
+                    colno:    err.colno,
+                    error:    err
+                });
+                reject(sensor, err);
+                sensor.dispatchEvent(errEvent);
+            });
+        }
+        
+        function activate(options) {
+            if (_watchId) {
+                navigator.geolocation.clearWatch(_watchId);
+            }
+            _watchId = navigator.geolocation.watchPosition(ondata, onerror, options);
+        }
+  
+        self.register = register;
+        self.deregister = deregister;
+        return self;
+    }());
+    
     var GeolocationSensorReading = (function() {
         function GeolocationSensorReading(dict) {
             this.accuracy         = dict.accuracy;
@@ -98,28 +236,40 @@
         return GeolocationSensorReading;
     })();
 
-
+    var privateData = new WeakMap();
+    function setSlot(self, name, value) {
+        var priv = privateData.get(self) || {};
+        priv[name] = value;
+        privateData.set(self, priv);
+        console.log("setSlot", name, value)
+    
+    }
+    function getSlot(self, name) {
+        var priv = privateData.get(self) || {};
+        console.log("getSlot", name, priv[name])
+        return priv[name];
+    }
+    
+    function reject(self, error) {
+        var rj = getSlot(self, "_startPromiseReject");
+        setSlot(self, "state", "idle");
+        setSlot(self, "_startPromise", null);
+        setSlot(self, "_startPromiseResolve", null);
+        setSlot(self, "_startPromiseReject", null);
+        self.reading = null;
+        rj(error);
+    }
+    
+    function resolve(self, reading) {
+        var resolve = getSlot(self, "_startPromiseResolve");
+        setSlot(self, "state", "active");
+        setSlot(self, "_startPromiseResolve", null);
+        setSlot(self, "_startPromiseReject", null);
+        self.reading = reading;
+        resolve();
+    }
     var GeolocationSensor = (function () {
-        var privateData = new WeakMap();
-        function setSlot(self, name, value) {
-            var priv = privateData.get(self) || {};
-            priv[name] = value;
-            privateData.set(self, priv);
-            console.log("setSlot", name, value)
         
-        }
-        function getSlot(self, name) {
-            var priv = privateData.get(self) || {};
-            console.log("getSlot", name, priv[name])
-            return priv[name];
-        }
-        function cleanup(self) {
-            setSlot(self, "state", "idle"); // one of idle/starting/reporting
-            setSlot(self, "_watchId", null);
-            setSlot(self, "_startPromise", null);
-            setSlot(self, "_startPromiseReject", null);
-            self.reading = null;
-        }
         function GeolocationSensor(options) {
             setSlot(this, "options", options);
             setSlot(this, "state", "idle");
@@ -132,79 +282,25 @@
             console.log(getSlot(this, "state"))
             if (getSlot(this, "state") == "idle") {
                 var self = this;
-                setSlot(self, "state", "starting");
+                setSlot(self, "state", "activating");
+                _geolocationSensor.register(sensor);
                 setSlot(self, "_startPromise", new Promise(function(resolve, reject) {
+                    setSlot(self, "_startPromiseResolve", resolve);
                     setSlot(self, "_startPromiseReject", reject);
-                
-                
-                    function onreading(position) {
-                        var state = getSlot(self, "state");
-                        if (state == "idle") {
-                            // consumer no longer interested in sensor output
-                            // stop right here.
-                            return;
-                        }
-                    
-                        if (state == "starting") {
-                            // This is the first event after startup
-                            setSlot(self, "state", "reporting");
-                            resolve();
-                        }
-                    
-                        var coords = position.coords;
-                        var reading = new GeolocationSensorReading({
-                            accuracy        : coords.accuracy,
-                            altitude        : coords.altitude,
-                            altitudeAccuracy: coords.altitudeAccuracy,
-                            heading         : coords.heading,
-                            latitude        : coords.latitude,
-                            longitude       : coords.longitude,
-                            speed           : coords.speed,
-                            timeStamp       : position.timestamp // watch out for the diff casing, here.
-                        });
-                    
-                        self.reading = reading;
-                        var event = new SensorReadingEvent("reading", {
-                            reading: reading
-                        });
-                        self.dispatchEvent(event);
-                    }
-        
-                    function onerror(err) {
-                        var errEvent = new ErrorEvent("error", {
-                            message:  err.message,
-                            filename: err.filename,
-                            lineno:   err.lineno,
-                            colno:    err.colno,
-                            error:    err
-                        });
-                        cleanup(self);
-                        self.dispatchEvent(errEvent);
-                        reject(err);
-                    }
-                
-                    setSlot(self, "_watchId", navigator.geolocation.watchPosition(onreading, onerror, {
-                        enableHighAccuracy: getSlot(self, "options").accuracy == "high", 
-                        maximumAge: 0, 
-                        timeout: Infinity
-                    }));
                 }));
             }
             return getSlot(this, "_startPromise");
         };
     
         GeolocationSensor.prototype.stop = function() {
-            var state = getSlot(self, "state");
+            var state = getSlot(this, "state");
             if (state == "idle") {
                 return;
-            }        
-        
-            navigator.geolocation.clearWatch(getSlot(this, "_watchId"));
-            cleanup(this); // TODO cleanup needs cleanup
-        
-            if (state == "starting") {
-                var reject = getSlot(this, "_startPromiseReject");
-                reject(new Error("abort")); // TODO improve error message
+            }
+            setSlot(this, "state", "idle");
+            _geolocationSensor.deregister(this);
+            if (state == "activating") {
+                reject(this, new Error("abort")); // TODO improve error message
             }
         };
     
@@ -224,3 +320,31 @@
 
 
 
+//window.GeolocationSensor.requestData = function(options) {
+//    return new Promise(function(resolve, reject) {
+//        function onsuccess(position) {
+//            resolve({
+//                latitude: position.coords.latitude,
+//                longitude: position.coords.longitude,
+//                accuracy: position.coords.accuracy
+//            });
+//        }
+//        
+//        function onerror(err) {
+//            if (err.code === err.TIMEOUT) {
+//                resolve(null);
+//            } else {
+//                reject(err);
+//            }
+//        }
+//        var timeout = Infinity;
+//        if ("timeout" in options) timeout = options.timeout;
+//        if (options.fromCache) timeout = 0; // instant timeout to only get data from the cache
+//        
+//        navigator.geolocation.getCurrentPosition(onsuccess, onerror, {
+//            enableHighAccuracy: options.accuracy == "high", 
+//            maximumAge: options.maximumAge == null ? Infinity : options.maximumAge,
+//            timeout: timeout
+//        });
+//    });
+//}
