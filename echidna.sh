2a4112330ca1a9717f95d5b3d00daa34edfc04e6
Anssi Kostiainen <anssi.kostiainen@gmail.com>
2017-08-09 16:29:14 +0300

    Update echidna.sh

--- echidna.sh
+++ echidna.sh
@@ -6,4 +6,5 @@
 DECISION="https://lists.w3.org/Archives/Public/public-device-apis/2015Oct/att-0037/minutes-2015-10-15.html#item05"
 
 # use --force option to allow building the spec despite not having a level.
-output=$(bikeshed --force echidna --u $W3C_USERNAME --p $W3C_PASSWORD  --d $DECISION --md-status=WD --self-contained)
+# UPDATE: --force not needed when Level: none
+output=$(bikeshed echidna --u $W3C_USERNAME --p $W3C_PASSWORD  --d $DECISION --md-status=WD)
