82a0944c5a248183fa71f1ab7b38e403ea8aa860
Fuqiao Xue <xfq@w3.org>
2019-09-28 09:17:11 +0800

    Bikeshed auto-deployment, add usecases.bs

--- compile.sh
+++ compile.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -e # Exit with nonzero exit code if anything fails
 
-bikeshed spec
+bikeshed spec index.bs
+bikeshed spec usecases.bs
 
 if [ -d out ]; then
-  echo Copy the generated spec into out/index.html
+  echo Copy the generated spec into out/*.html
   cp index.html out/index.html
+  cp usecases.html out/usecases.html
 fi
